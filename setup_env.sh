86ae29d46a7a123f35bf3a713d93edae3afce2fa
Debanjan Datta <32544780+ddatta-DAC@users.noreply.github.com>
2017-10-12 20:44:24 -0400

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-sh Miniconda2-latest-Linux-x86_64.sh 
-
-conda install -c conda-forge pygrib
-conda install -c conda-forge requests-ftp
-conda install --channel https://conda.anaconda.org/anaconda mysql-python
-conda install -c conda-forge dateutils
-conda install -c anaconda pymongo 
-
-
-#Tensorflow
-conda install -c conda-forge tensorflow 
-
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
 
 
 
