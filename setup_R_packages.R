2e325fb81ed711c133b96a14e44f86a59399340b
Debanjan Datta <32544780+ddatta-DAC@users.noreply.github.com>
2017-10-31 09:56:50 -0400

    Update setup_R_packages.R

--- setup_R_packages.R
+++ setup_R_packages.R
@@ -1,9 +1,9 @@
 # Launch R : R
-install.package("curl")
-install.package("openssl")
+install.packages("curl")
+install.packages("openssl")
 install.packages("devtools")
 library(devtools)
 install_github("igraph/rgraph")
-install.package("Matrix")
+install.packages("Matrix")
 install.packages("MASS")
 install.packages("genlasso")
